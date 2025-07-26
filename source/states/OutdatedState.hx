042858ccc3edf11ff817f49a3d227521bed00f2f
Andre Nicholas Jr <151701402+Andrhevn23Rbx@users.noreply.github.com>
2025-07-26 13:33:42 -0500

    Update OutdatedState.hx
    e

--- source/states/OutdatedState.hx
+++ source/states/OutdatedState.hx
@@ -1,9 +1,22 @@
-override function create()
+package states;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.FlxState;
+
+class OutdatedState extends FlxState
 {
-	super.create();
+    override function create()
+    {
+        super.create();
 
-	// Instantly skip to main menu without showing anything
-	leftState = true;
-	MusicBeatState.switchState(new MainMenuState());
+        var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+        add(bg);
+
+        var text:FlxText = new FlxText(0, FlxG.height / 2 - 10, FlxG.width, "This version is outdated.");
+        text.setFormat(null, 16, FlxColor.WHITE, "center");
+        add(text);
+    }
 }
-
